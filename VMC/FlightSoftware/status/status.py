4e57395f8f247df92dc7bd833f40ed4760328171
Nathan Vaughn <nvaughn@bellflight.com>
2022-04-07 08:50:49 -0500

    Refactor status module

--- VMC/FlightSoftware/status/status.py
+++ VMC/FlightSoftware/status/status.py
@@ -1,17 +1,17 @@
 import itertools
-import json
 import subprocess
-import threading
 import time
-from typing import Any, Optional
+from typing import Any, Dict, Optional, Tuple
 
 import board
 import neopixel_spi as neopixel
 import paho.mqtt.client as mqtt
 from loguru import logger
+from mqtt_library import MQTTModule
 
 NUM_PIXELS = 12
 PIXEL_ORDER = neopixel.GRB
+
 # RGB
 COLORS = (0xFF0000, 0x00FF00, 0x0000FF)
 CLR_PURPLE = 0x6A0DAD
@@ -29,30 +29,21 @@ APRIL_LED = 5
 DELAY = 0.1
 
 
-class Status(object):
-    def __init__(self) -> None:
+class StatusModule(MQTTModule):
+    def __init__(self):
+        super().__init__()
+
         self.initialized = False
-        self.mqtt_host = "mqtt"
-        self.mqtt_port = 18830
-
-        self.mqtt_client = mqtt.Client()
-
-        self.mqtt_client.on_connect = self.on_connect
-        self.mqtt_client.on_message = self.on_message
-
-        self.topic_prefix = "vrc/status"
 
         self.topic_map = {
-            "vrc/status/light_status": self.light_status,
-            "vrc/status/light_status_PCC": self.light_status,
-            "vrc/status/light_status_VIO": self.light_status,
-            "vrc/status/light_status_APRIL": self.light_status,
-            "vrc/status/light_status_FCC": self.light_status,
-            "vrc/status/light_status_THERMAL": self.light_status,
+            "vrc/status/light_pcm": self.light_status,
+            "vrc/status/light_vio": self.light_status,
+            "vrc/status/light_apriltags": self.light_status,
+            "vrc/status/light_fcm": self.light_status,
+            "vrc/status/light_thermal": self.light_status,
         }
 
         self.spi = board.SPI()
-
         self.pixels = neopixel.NeoPixel_SPI(
             self.spi, NUM_PIXELS, pixel_order=PIXEL_ORDER, auto_write=False
         )
@@ -62,54 +53,43 @@ class Status(object):
     def on_message(
         self, client: mqtt.Client, userdata: Any, msg: mqtt.MQTTMessage
     ) -> None:
-        try:
-            # logger.debug(f"{msg.topic}: {str(msg.payload)}")
-            self.check_status(msg.topic)
-
-            if msg.topic in self.topic_map:
-                payload = json.loads(msg.payload)
-                self.topic_map[msg.topic](payload)
-
-        except Exception as e:
-            logger.exception(f"Error handling message on {msg.topic}")
+        # run this function on every message recieved before processing topic map
+        self.check_status(msg.topic)
+        super().on_message(client, userdata, msg)
 
     def on_connect(
         self,
         client: mqtt.Client,
         userdata: Any,
-        rc: int,
+        rc: Any,
         properties: Optional[mqtt.Properties] = None,
     ) -> None:
-        logger.debug(f"SYSTEM STATUS MODULE : Connected with result code {rc}")
-        for topic in self.topic_map.keys():
-            logger.debug(f"STATUS: Subscribed to: {topic}")
-            client.subscribe(topic)
+        super().on_connect(client, userdata, rc, properties)
+        # additionally subscribe to all topics
         client.subscribe("vrc/#")
 
     def set_cpu_status(self) -> None:
-
         ## Initialize power mode status
-
-        batcmd = "/app/nvpmodel --verbose -f /app/nvpmodel.conf -m 0"
+        cmd = ["/app/nvpmodel", "--verbose", "-f", "/app/nvpmodel.conf", "-m", "0"]
         try:
-            subprocess.check_output(batcmd, shell=True, stderr=subprocess.STDOUT)
+            subprocess.check_call(cmd, stderr=subprocess.STDOUT)
         except subprocess.CalledProcessError as e:
             logger.exception(
-                f"command '{e.cmd}' return with error (code {e.returncode}): {e.output}"
+                f"Command '{e.cmd}' return with error ({e.returncode}): {e.output}"
             )
 
     def check_status(self, topic: str) -> None:
-        # logger.debug(f"check status: {str(topic)}")
-        if "vrc/vio" in topic:
-            self.light_up(VIO_LED, CLR_PURPLE)
-        if "vrc/pcc" in topic:
-            self.light_up(PCC_LED, CLR_AQUA)
-        if "vrc/fcc" in topic:
-            self.light_up(FCC_LED, CLR_ORANGE)
-        if "vrc/thermal" in topic:
-            self.light_up(THERMAL_LED, CLR_BLUE)
-        if "vrc/april" in topic:
-            self.light_up(APRIL_LED, CLR_YELLOW)
+        lookup: Dict[str, Tuple[int, int]] = {
+            "vrc/vio": (VIO_LED, CLR_PURPLE),
+            "vrc/pcm": (PCC_LED, CLR_AQUA),
+            "vrc/fcm": (FCC_LED, CLR_ORANGE),
+            "vrc/thermal": (THERMAL_LED, CLR_BLUE),
+            "vrc/apriltags": (APRIL_LED, CLR_YELLOW),
+        }
+
+        for key, value in lookup.items():
+            if topic.startswith(key):
+                self.light_up(*value)
 
     def red_status_all(self) -> None:
         for i in range(NUM_PIXELS):
@@ -120,7 +100,7 @@ class Status(object):
         self.pixels[which_one] = color
         self.pixels.show()
 
-    def light_status(self, msg: dict) -> None:
+    def light_status(self, payload: dict) -> None:
         for color, i in itertools.product(COLORS, range(NUM_PIXELS)):
             self.pixels[i] = color
             self.pixels.show()
@@ -129,38 +109,29 @@ class Status(object):
 
     def status_check(self) -> None:
         if not self.initialized:
-            self.initialized = True
             self.set_cpu_status()
+            self.initialized = True
 
-        batcmd = "/app/nvpmodel -f /app/nvpmodel.conf -q"
+        cmd = ["/app/nvpmodel", "-f", "/app/nvpmodel.conf", "-q"]
         try:
-            result = subprocess.check_output(
-                batcmd, shell=True, stderr=subprocess.STDOUT
+            result = subprocess.check_output(cmd, stderr=subprocess.STDOUT).decode(
+                "utf-8"
             )
-            self.pixels[0] = COLORS[1] if b"MAXN" in result else COLORS[0]
+            self.pixels[0] = COLORS[1] if "MAXN" in result else COLORS[0]
             self.pixels.show()
         except subprocess.CalledProcessError as e:
             logger.exception(
-                f"command '{e.cmd}' return with error (code {e.returncode}): {e.output}"
+                f"Command '{e.cmd}' return with error ({e.returncode}): {e.output}"
             )
 
-    def status_thread(self) -> None:
+    def run(self) -> None:
+        self.run_non_blocking()
+
         while True:
             self.status_check()
             time.sleep(1)
 
-    def run(self) -> None:
-        # allows for graceful shutdown of any child threads
-        self.mqtt_client.connect(host=self.mqtt_host, port=self.mqtt_port, keepalive=60)
-
-        status_thread = threading.Thread(
-            target=self.status_thread, args=(), daemon=True, name="request_status_check"
-        )
-        status_thread.start()
-
-        self.mqtt_client.loop_forever()
-
 
 if __name__ == "__main__":
-    status = Status()
+    status = StatusModule()
     status.run()
