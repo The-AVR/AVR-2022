cb78a3f318c7e5b9ffbcaf666bec543da54fcd63
Lance Riedel <lanceriedel@gmail.com>
2022-07-21 09:52:43 -0500

    gui input for min/max thermal camera (#136)
    * gui input for min/max thermal camera
    
    * removed semicolons
    
    * changed to setText and fixed whitespace
    
    * ran formatting
    
    * ran import sort
    
    * added type ignore to clicked
    
    * formatting
    
    * unused import

--- GUI/app/tabs/thermal_view_control.py
+++ GUI/app/tabs/thermal_view_control.py
@@ -12,6 +12,7 @@ from bell.avr.mqtt.payloads import AvrPcmFireLaserPayload, AvrPcmSetServoPctPayl
 from PySide6 import QtCore, QtGui, QtWidgets
 
 from ..lib.calc import constrain
+from ..lib.widgets import DoubleLineEdit
 from .base import BaseTabWidget
 
 
@@ -88,6 +89,10 @@ class ThermalView(QtWidgets.QWidget):
 
         # need a bit of padding for the edges of the canvas
         self.setFixedSize(self.width_ + 50, self.height_ + 50)
+
+    def set_temp_range(self, mintemp: float, maxtemp: float) -> None:
+        self.MINTEMP = mintemp
+        self.MAXTEMP = maxtemp
 
     def update_canvas(self, pixels: List[int]) -> None:
         float_pixels = [
@@ -286,6 +291,31 @@ class ThermalViewControlWidget(BaseTabWidget):
 
         self.viewer = ThermalView(self)
         viewer_layout.addWidget(self.viewer)
+
+        # set temp range
+
+        # lay out the host label and line edit
+        temp_range_layout = QtWidgets.QFormLayout()
+
+        self.temp_min_line_edit = DoubleLineEdit()
+        temp_range_layout.addRow(QtWidgets.QLabel("Min Temp:"), self.temp_min_line_edit)
+        self.temp_min_line_edit.setText(str(self.viewer.MINTEMP))
+
+        self.temp_max_line_edit = DoubleLineEdit()
+        temp_range_layout.addRow(QtWidgets.QLabel("Max Temp:"), self.temp_max_line_edit)
+        self.temp_max_line_edit.setText(str(self.viewer.MAXTEMP))
+
+        set_temp_range_button = QtWidgets.QPushButton("Set Temp Range")
+        temp_range_layout.addWidget(set_temp_range_button)
+
+        viewer_layout.addLayout(temp_range_layout)
+
+        set_temp_range_button.clicked.connect(  # type: ignore
+            lambda: self.viewer.set_temp_range(
+                float(self.temp_min_line_edit.text()),
+                float(self.temp_max_line_edit.text()),
+            )
+        )
 
         layout.addWidget(viewer_groupbox)
 
