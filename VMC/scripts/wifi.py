774a033051db0a405b848c38e27c2222910ae32e
noelified <32148064+noelbrownback@users.noreply.github.com>
2022-08-02 15:27:42 -0500

    Reverting to old

--- VMC/scripts/wifi.py
+++ VMC/scripts/wifi.py
@@ -90,7 +90,7 @@ def create():
     # create
     print(f"===== Creating network {ssid} with password {password} =====")
     subprocess.check_call(["nmcli", "device", "wifi", "hotspot", "ifname", "wlan0", "ssid", ssid, "password", password])
-    subprocess.check_call(["nmcli", "connection", "modify", "Hotspot", "connection.autoconnect", "true"])
+    subprocess.check_call(["nmcli", "connection", "modify", "Hotspot", "connection.autoconnect", "yes"])
 
 def status():
     """
