6377047448bb7b05c0405f685e27c7687287e7a6
Nathan Vaughn <nvaughn51@gmail.com>
2022-09-16 19:31:50 -0500

    Add option for inverted gimbal joystick

--- GUI/app/lib/calc.py
+++ GUI/app/lib/calc.py
@@ -14,3 +14,9 @@ def normalize_value(value: float, min_value: float, max_value: float) -> float:
     value_range = max_value - min_value
     relative_value = value - min_value
     return relative_value / value_range
+
+
+def map_value(
+    x: float, in_min: float, in_max: float, out_min: float, out_max: float
+) -> float:
+    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
