camera rotation fix

GUI/app/tabs/thermal_view_control.py
@@ -69,8 +69,6 @@ class ThermalView(QtWidgets.QWidget):
             for ix in range(self.camera_total)
         ]
 
-        self.points = self.rotate90Clockwise(self.points)
-
         # i'm not fully sure what this does
         self.grid_x, self.grid_y = np.mgrid[
             0 : self.camera_x - 1 : self.camera_total / 2j,
@@ -112,8 +110,12 @@ class ThermalView(QtWidgets.QWidget):
             for p in pixels
         ]
 
+        float_pixels_matrix = np.reshape(float_pixels, (8,8))
+        float_pixels_matrix = np.rot90(float_pixels_matrix, 1)
+        rotated_float_pixels= float_pixels_matrix.flatten()
+
         bicubic = scipy.interpolate.griddata(
-            self.points, float_pixels, (self.grid_x, self.grid_y), method="cubic"
+            self.points, rotated_float_pixels, (self.grid_x, self.grid_y), method="cubic"
         )
 
         pen = QtGui.QPen(QtCore.Qt.NoPen)
@@ -134,16 +136,6 @@ class ThermalView(QtWidgets.QWidget):
                     pen,
                     brush,
                 )
-    def rotate90Clockwise(A):
-        N = len(A[0])
-        for i in range(N // 2):
-            for j in range(i, N - i - 1):
-                temp = A[i][j]
-                A[i][j] = A[N - 1 - j][i]
-                A[N - 1 - j][i] = A[N - 1 - i][N - 1 - j]
-                A[N - 1 - i][N - 1 - j] = A[j][N - 1 - i]
-                A[j][N - 1 - i] = temp
-
 
 class JoystickWidget(BaseTabWidget):
     def __init__(self, parent: QtWidgets.QWidget) -> None: